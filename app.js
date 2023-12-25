aec48d80f51ca43ab3aae87fef950243bba8b3c8
Jonathan <jonathankendrick697@gmail.com>
2023-12-25 01:47:51 -0800

    second commit

--- app.js
+++ app.js
@@ -23,57 +23,28 @@ const dom = (() => {
     }, 1500);
   };
 
-  const displayMood = () => {
-    let finalCard = document.getElementById('final-card');
-    if (getSelectedRadioBtn() === 'yes') {
-      finalCard.innerHTML = '';
-      finalCard.style.backgroundImage =
-        'url("https://media.giphy.com/media/xT5LMHxhOfscxPfIfm/giphy.gif")';
-    } else if (getSelectedRadioBtn() === 'no') {
-      finalCard.innerHTML = '';
-      finalCard.style.backgroundImage =
-        'url("https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif")';
-    }
-  };
-
   return {
     displayApologyCard,
     displayFinalCard,
-    displayMood,
   };
 })();
 
 /* ---------- */
-const animation = (() => {
-  /* fade down */
+const fadeInIntroCard = () => {
   setTimeout(() => {
     document.querySelector('.intro-card').classList.add('active');
   }, 1300);
+};
 
-  /* fade in apology later */
-})();
-
-function getSelectedRadioBtn() {
-  let radioBtns = document.querySelectorAll('.radio-btn');
-  for (let i = 0; i < radioBtns.length; i++) {
-    if (radioBtns[i].checked) {
-      let selectedValue = radioBtns[i].value;
-      return selectedValue;
-    }
-  }
-}
-
-const sendMessage = (() => {})();
+fadeInIntroCard();
 
 // events
 let introBtn = document.getElementById('intro-btn');
-introBtn.addEventListener('click', dom.displayApologyCard);
+if (introBtn) {
+  introBtn.addEventListener('click', dom.displayApologyCard);
+}
 
 let outroBtn = document.getElementById('outro-btn');
-outroBtn.addEventListener('click', dom.displayFinalCard);
-
-let finalBtn = document.getElementById('final-btn');
-finalBtn.addEventListener('click', (e) => {
-  // e.preventDefault();
-  // dom.displayMood();
-});
+if (outroBtn) {
+  outroBtn.addEventListener('click', dom.displayFinalCard);
+}
